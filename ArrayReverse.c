c2211a6ad74da9984fbf5e0a8436eda9f004a81f
kapooky <kapooky101@gmail.com>
2017-07-05 00:27:53 -0400

    Create ArrayReverse.c
    Hi WALKA ShiiiiSA

---
+++ ArrayReverse.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+// Creates array of size n, and inserts number into the n slots, then prints number backwards.
+int main()
+{
+	
+	//Declare int to determine size of array 
+    int a;
+    printf("Size of int = %ld bytes \n", sizeof(a));
+    
+    scanf("%d", &a); 
+    printf("\n %d", a);
+    
+    //create an array of size a
+    int array[a];
+    printf("\n Size of array: %d", sizeof(array)); 
+    
+    
+    int i;
+    //insert desired number into the array 
+    for(i = 0; i < (sizeof(array)/(sizeof(int))); ++i){
+    	printf("Insert the number in here: ");
+		scanf("%d",&array[i]);  
+    	
+	}
+	//Reverse the ordering 
+	 for(i = (sizeof(array)/(sizeof(int))) - 1 ; i >= 0 ; i--){
+	 	printf("\n %d",array[i]); 
+	 }
+    
+  
+
+
+    return 0;
+}
