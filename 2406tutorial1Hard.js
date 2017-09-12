6b1691ed0c6238ceeaed4e1af92a4bed03b76d5e
kapooky <kapooky101@gmail.com>
2017-09-12 19:24:14 -0400

    Create 2406tutorial1Hard.js

---
+++ 2406tutorial1Hard.js
@@ -0,0 +1,36 @@
+/*
+Hello World example to serving string to client
+*/
+//To test use browser to view http://localhost:3000
+//Cntl+C to stop server
+
+var http = require('http');
+var counter = 1000;
+
+http.createServer(function (request,response){
+   //respond to client
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.write('Hello\n');
+
+ var urlString = request.url;
+ var index = urlString.indexOf("?name=");
+ if(index > 0) {
+
+
+  var name = urlString.substring(index + "?name=".length, urlString.length);
+index1 = name.indexOf("&")
+if(index1 > 0 ){
+  console.log("This name is big");
+  name = name.substring(0,index1 );
+}
+
+  response.write(name + "\n");
+  }
+ else response.write('World\n');
+   //end HTTP response and provide final data to send
+   response.write('Faggotry should be condemmed ')
+   response.end("["+ counter++ + "]\n");
+}).listen(3000, "127.0.0.1");
+console.log('Server Running at http://127.0.0.1:3000  CNTL-C to quit');
+console.log('To test with browser visit: http://localhost:3000');
+console.log("hello");
